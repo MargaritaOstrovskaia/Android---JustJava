d7a8f364f01f97b612b276116e67ae137e91e2e4
Margarita Ostrovskaia <margarita.v.ostrovskaya@gmail.com>
2020-04-05 19:31:47 -0700

    add style, font, background

--- JustJava/app/src/main/java/com/example/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/justjava/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.justjava;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Display;
@@ -10,16 +11,52 @@ import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 /**
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
+    private boolean addWhippedCream = false;
+    private boolean addChocolate = false;
     private int quantity = 1;
+    private int totalPrice = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CheckBox whippedCreamCheckBox = findViewById(R.id.whipped_cream_checkbox);
+        whippedCreamCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                addWhippedCream = isChecked;
+                setTotalPrice();
+            }
+        });
+
+        CheckBox chocolateCheckBox = findViewById(R.id.chocolate_checkbox);
+        chocolateCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                addChocolate = isChecked;
+                setTotalPrice();
+            }
+        });
+
+        setTotalPrice();
+    }
+
+    /**
+     * This method set total price in TextView.
+     */
+    private void setTotalPrice() {
+        totalPrice = calculatePrice();
+        String priceText = getString(R.string.total_price, totalPrice);
+
+        TextView totalPriceTextView = findViewById(R.id.total_price_text_view);
+        totalPriceTextView.setText(priceText);
     }
 
     /**
@@ -30,24 +67,13 @@ public class MainActivity extends AppCompatActivity {
         EditText nameTextEdit = findViewById(R.id.name_text_edit);
         String name = nameTextEdit.getText().toString();
 
-        // Figure out if whe user wants whipped cream topping
-        CheckBox whippedCreamCheckBox = findViewById(R.id.whipped_cream_checkbox);
-        boolean addWhippedCream = whippedCreamCheckBox.isChecked();
-
-        // Figure out if whe user wants chocolate topping
-        CheckBox chocolateCheckBox = findViewById(R.id.chocolate_checkbox);
-        boolean addChocolate = chocolateCheckBox.isChecked();
-
-        // Calculate the price
-        int price = calculatePrice(5, addWhippedCream, addChocolate);
-
         // Get order summary message
-        String priceMessage = createOrderSummary(name, price, addWhippedCream, addChocolate);
+        String priceMessage = createOrderSummary(name);
 
         // Use an intent to launch an email app.
         // Send the order summary in the email body.
-        Intent intent = new Intent(Intent.ACTION_SEND);
-        intent.setType("*/*");
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
         intent.putExtra(Intent.EXTRA_SUBJECT, "Coffee order");
         intent.putExtra(Intent.EXTRA_TEXT, priceMessage);
 
@@ -57,12 +83,11 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * Calculates the price of the order.
-     * @param basePrice price of 1 cup coffee.
-     * @param addWhippedCream is whether or not the user wants whipped cream topping.
-     * @param addChocolate is whether or not the user wants chocolate topping.
      * @return total price.
      */
-    private int calculatePrice(int basePrice, boolean addWhippedCream, boolean addChocolate) {
+    private int calculatePrice() {
+        int basePrice = 5;
+
         if (addWhippedCream)
             basePrice += 1;
         if (addChocolate)
@@ -74,12 +99,9 @@ public class MainActivity extends AppCompatActivity {
     /**
      * Create summary of the order.
      * @param name on the order.
-     * @param price on the order.
-     * @param addWhippedCream is whether or not the user wants whipped cream topping.
-     * @param addChocolate is whether or not the user wants chocolate topping.
      * @return text summary.
      */
-    private String createOrderSummary(String name, int price, boolean addWhippedCream, boolean addChocolate) {
+    private String createOrderSummary(String name) {
         String whippedCream;
         if (addWhippedCream)
             whippedCream = getString(R.string.answer_yes);
@@ -92,7 +114,7 @@ public class MainActivity extends AppCompatActivity {
         else
             chocolate = getString(R.string.answer_no);
 
-        return getString(R.string.price_val, name, whippedCream, chocolate, quantity, price);
+        return getString(R.string.price_val, name, whippedCream, chocolate, quantity, totalPrice);
     }
 
     /**
@@ -101,6 +123,8 @@ public class MainActivity extends AppCompatActivity {
     public void increment(View view) {
         if (quantity < 100)
             quantity++;
+        setQuantity();
+        setTotalPrice();
     }
 
     /**
@@ -109,5 +133,15 @@ public class MainActivity extends AppCompatActivity {
     public void decrement(View view) {
         if (quantity > 0)
             quantity--;
+        setQuantity();
+        setTotalPrice();
+    }
+
+    /**
+     * Change quantity value in TextView.
+     */
+    private void setQuantity() {
+        TextView textViewQuantity = findViewById(R.id.quantity_text_view);
+        textViewQuantity.setText(String.valueOf(quantity));
     }
 }
